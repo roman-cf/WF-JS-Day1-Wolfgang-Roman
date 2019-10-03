16ae98be937855ad152248e8f60952630803004e
roman.cf <roman.springer@outlook.com>
2019-10-03 10:04:47 +0200

    ex 3 fertig

--- basic/baex2.js
+++ basic/baex2.js
@@ -1,1 +1,5 @@
 console.log("Hi my name is Martin");
+var text = "Hi my name is Martin";
+var alter = 25;
+
+document.getElementById("body").innerHTML = text + " and I am " + alter + " years old."
---
+++ basic/baex3.js
@@ -0,0 +1,6 @@
+console.log("Hi my name is Martin");
+var text = "Hi my name is Martin";
+var alter = 25;
+
+
+document.getElementById("dieID").innerHTML = text + " and I am " + alter + " years old.";
