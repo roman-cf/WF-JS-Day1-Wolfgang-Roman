9ef1a4791717134d069f4065f391033eb11ae7ef
roman.cf <roman.springer@outlook.com>
2019-10-03 13:33:37 +0200

    advanced 1 fertig

---
+++ Advanced/adv11.js
@@ -0,0 +1,20 @@
+var a = 7;
+var b = 14;
+var c = "21";
+var d = '36';
+var e = 42;
+
+sum = Number(a)+Number(b)+Number(c)+Number(d)+Number(e);
+
+console.log(sum);
+
+var f = '1';
+var g = 15;
+var h = 8;
+var i = "1";
+
+product = Number(f)*Number(g)*Number(h)*Number(i);
+
+console.log(product);
+
+document.getElementById('dieID').innerHTML = sum/product;
---
+++ Advanced/adv12.js
@@ -0,0 +1,7 @@
+var myArr = [[1,2,1,24],[8,11,9,4],[7,0,7,27],[7,4,28,14],[3,10,25,7],[21,4,6,17],[3,5,26,3]];
+
+console.log(myArr[1][1]);
+console.log(myArr[4][2]);
+console.log(myArr[5][3]);
+console.log(myArr[2][3]);
+console.log(myArr[2][1]);
