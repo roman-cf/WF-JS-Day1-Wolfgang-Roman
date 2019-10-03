d04d854e02cfe616e51a0fe9fde02cdd2935f692
Wolfgang Figl <wolfgang.figl@me.com>
2019-10-03 14:26:48 +0200

    adv22

--- Advanced/adv22.js
+++ Advanced/adv22.js
@@ -1,4 +1,27 @@
-var myYesterday = (function(){
+var dateObj = new Date();
+
+dateObj.setDate(dateObj.getDate() -1);
+
+document.getElementById("bob").innerHTML = dateObj;
 
 
-}());
+
+
+
+
+
+
+
+	/*var todaytimestamp = +new Date();
+	var oneDayTimeStamp = 1000 * 60 * 60 * 24;
+	var diff = todaytimestamp - oneDayTimeStamp;
+	var yesterdayDate = new Date(diff);
+	var yesterdayString = yesterdayDate.getFullYear() + '-' + (yesterdayDate.getMonth() +1) + '-' + yesterdayDate.getDate();
+	alert(yesterdayString);*/
+	
+
+
+
+
+
+
