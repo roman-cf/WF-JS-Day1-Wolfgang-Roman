8c740e276633ffcf10059b14d18e3bbd74cca385
roman.cf <roman.springer@outlook.com>
2019-10-03 10:40:28 +0200

    ex 2.2 fertig

---
+++ basic/baex22.js
@@ -0,0 +1,13 @@
+
+ageCalc(1999);
+
+function ageCalc(birt_year){
+	
+	var heuer = new Date();
+	var yourage = (heuer.getFullYear())-birt_year;
+	document.getElementById("dieID").innerHTML 	= "You ar either "
+												+ (yourage - 1)
+												+ " or "
+												+ yourage
+												+ ".";	
+}
